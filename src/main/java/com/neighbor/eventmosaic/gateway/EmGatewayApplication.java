acf5c6dafd489f3b84c1049605ec782e850862f2
konstantin <mailspace.hub@gmail.com>
2025-01-26 22:21:35 +0300

    Инициализация, базовые конфиги, регистрация в Eureka

---
+++ src/main/java/com/neighbor/eventmosaic/gateway/EmGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.neighbor.eventmosaic.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmGatewayApplication.class, args);
+	}
+
+}
