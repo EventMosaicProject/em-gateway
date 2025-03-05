d1d9af73291e06d809f6e426069501cd55d3ffa8
konstantin <mailspace.hub@gmail.com>
2025-03-05 13:51:11 +0300

    Добавлена зависимость и конфиги для prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 
 	compileOnly("org.projectlombok:lombok")
 
